73a567b9944d2e55945a4720b29dbe415930af88
AkaiBureido <utkin.oleg@me.com>
2013-07-04 01:53:26 +0400

    Tidy up

--- popup.js
+++ popup.js
@@ -69,30 +69,30 @@ function JEViewController() {
   }
 
   this.switchToEmoticonTableView = function (categoryName, subcategoryName) {
-      this.setTitle( this._sym_to_str(subcategoryName) );
-      this.setBackButton( function(e){
-        this.switchToSubCategoryView(categoryName);
-      }.bind(this));
-      this.clearViewContainer();
-      
-      emoticons = this.Model.getEmoticonList(path[0],path[1]);
-     
-      this.TableView.render(emoticons, this, function(smiley, e) {
-        console.log(smiley);
-        this.copyToClipboard(smiley);
-      });
+    this.setTitle( this._sym_to_str(subcategoryName) );
+    this.setBackButton( function(e){
+      this.switchToSubCategoryView(categoryName);
+    }.bind(this));
+    this.clearViewContainer();
+    
+    emoticons = this.Model.getEmoticonList(path[0],path[1]);
+    
+    this.TableView.render(emoticons, this, function(smiley, e) {
+      console.log(smiley);
+      this.copyToClipboard(smiley);
+    });
   }
 
   this.copyToClipboard = function(newText) {
-      var copyDiv = document.createElement('div');
-      copyDiv.contentEditable = true;
-      document.body.appendChild(copyDiv);
-      copyDiv.innerHTML = newText;
-      copyDiv.unselectable = "off";
-      copyDiv.focus();
-      document.execCommand('SelectAll');
-      document.execCommand("Copy", false, null);
-      document.body.removeChild(copyDiv);
+    var copyDiv = document.createElement('div');
+    copyDiv.contentEditable = true;
+    document.body.appendChild(copyDiv);
+    copyDiv.innerHTML = newText;
+    copyDiv.unselectable = "off";
+    copyDiv.focus();
+    document.execCommand('SelectAll');
+    document.execCommand("Copy", false, null);
+    document.body.removeChild(copyDiv);
   }
 
   this._sym_to_str = function(sym) {
@@ -313,77 +313,3 @@ function JEModel() {
     }
   }
 }
-
-// var JapaneseEmoticons = {
-//   collection: "",
-//   collectionPresent: "",
-//   
-//   inilialise: function() {
-//     var req = new XMLHttpRequest();
-//     req.onload = this.parseEmoticons.bind(this);
-//     req.open("get", chrome.extension.getURL("emoticons.json"), true);
-//     req.send();
-//   },
-// 
-//   buildMENU: function(category) {
-//     var table = document.getElementById('emotes')
-//   },
-// 
-//   insertTextAtCarret: function(newText) {
-//     document.activeElement.setRangeText("(^_^)")
-//   },
-// 
-//   parseEmoticons: function(e) {
-//     this.collection = JSON.parse(e.target.responseText)
-//     this.buildMENU()
-//   },
-//   
-//   // Utility functions
-//   getCategories: function() {
-//     return Object.keys(this.collection)
-//   },
-// 
-//   getSubCategories: function(category) {
-//     return Object.keys(this.collection[category])
-//   },
-// 
-//   getEmoticonList: function(superCategory, category) {
-//     if(this.collection[superCategory]) {
-//       return this.collection[superCategory][category]['list']
-//     } else {
-//       return
-//     }
-//   },
-// 
-//   calculateTextWidth: function(string, parent) {
-//     var container = document.createElement('div')
-//     container.setAttribute('style', 'position: absolute; visibility: hidden; height: auto; width: auto;')
-// 
-//     parent.appendChild(container)
-// 
-//     var text = document.createTextNode(string)
-//     container.appendChild(text)
-// 
-//     var metrics = [ 
-//       (container.clientHeight + 1),
-//       (container.clientWidth + 1)
-//     ]
-// 
-//     parent.removeChild(container)
-// 
-//     return metrics
-//   }
-// }
-
-
-
-// a = {}
-// 
-// function reqListener () {
-//   a = this.responseText
-// };
-// 
-// var oReq = new XMLHttpRequest();
-// oReq.onload = reqListener;
-// oReq.open("get", chrome.extension.getURL("emoticons.json"), true);
-// oReq.send();
